46e709c5640cbaa6dd6c24e0dc16a770356d1bca
Micheal Slade <50627681+Micheal-Slade@users.noreply.github.com>
2023-10-19 21:26:22 +0100

    Variables loading in but bars are not correct.

--- src/components/BarChart.svelte
+++ src/components/BarChart.svelte
@@ -1,82 +1,76 @@
 <script>
     import { onMount } from 'svelte';
-    import { dataStore, fetchData } from '../store/store.js';
+    import { dataStore } from '../store/store.js';
     import * as d3 from 'd3';
   
-    let selectedVariable = '';
-    
+    let selectedVariable = 'IMD_Rank';  // Default variable
+  
     const updateChart = () => {
-      const margin = { top: 20, right: 30, bottom: 40, left: 90 },
-            width = 960 - margin.left - margin.right,
-            height = 500 - margin.top - margin.bottom;
+      dataStore.subscribe(({ data }) => {
+        // Set the dimensions and margins of the graph
+        const margin = {top: 20, right: 30, bottom: 40, left: 90},
+              width = 800 - margin.left - margin.right,
+              height = 400 - margin.top - margin.bottom;
   
-      // append the svg object to the body of the page
-      const svg = d3.select("#bar-chart")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        // Select the svg area
+        d3.select("#bar-chart-svg").html("");
   
-      dataStore.subscribe(storeValue => {
-        const data = storeValue.data.map(d => ({ name: d.LAD22NM, value: +d[selectedVariable] }));
+        // Append SVG
+        const svg = d3.select("#bar-chart-svg")
+          .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+            .attr("transform", `translate(${margin.left},${margin.top})`);
   
         // X axis
         const x = d3.scaleLinear()
-          .domain([0, d3.max(data, d => d.value)])
+          .domain([0, d3.max(data, d => d[selectedVariable])])
           .nice()
           .range([0, width]);
+  
         svg.append("g")
-          .attr("transform", "translate(0," + height + ")")
-          .call(d3.axisBottom(x))
-          .selectAll("text")
-            .attr("transform", "translate(-10,0)rotate(-45)")
-            .style("text-anchor", "end");
+          .attr("transform", `translate(0,${height})`)
+          .call(d3.axisBottom(x));
   
         // Y axis
         const y = d3.scaleBand()
-          .range([height, 0])
-          .domain(data.map(d => d.name))
+          .range([0, height])
+          .domain(data.map(d => d.LAD22NM))
           .padding(.1);
+  
         svg.append("g")
-          .call(d3.axisLeft(y))
-          .selectAll("text")
-            .attr("dy", null);
+          .call(d3.axisLeft(y));
   
         // Bars
-        svg.selectAll("rect")
+        svg.selectAll("myRect")
           .data(data)
           .enter()
           .append("rect")
-            .attr("x", x(0))
-            .attr("y", d => y(d.name))
-            .attr("width", d => x(d.value))
-            .attr("height", y.bandwidth())
-            .attr("fill", "#69b3a2");
+          .attr("x", x(0) )
+          .attr("y", d => y(d.LAD22NM))
+          .attr("width", d => x(d[selectedVariable]))
+          .attr("height", y.bandwidth() )
+          .attr("fill", "#69b3a2");
       })();
+    };
+  
+    const onVariableChange = (event) => {
+      selectedVariable = event.target.value;
+      updateChart();
     };
   
     onMount(updateChart);
   </script>
   
-  <!-- Dropdown for variable selection -->
-  <div>
-    <select bind:value={selectedVariable} on:change={updateChart}>
-      <option disabled selected value=""> -- select a variable -- </option>
-      <!-- Listing all variables except the location identifiers -->
-      <option>IMD</option>
-      <option>Teenage Deliveries</option>
-      <option>Children in Care</option>
-      <!-- ... add all other variables here ... -->
-    </select>
-  </div>
+  <!-- Search Function -->
+  <select on:change={onVariableChange}>
+    <option value="IMD_Rank">IMD Rank</option>
+    <!-- Add other options here for each variable you want to make selectable -->
+    {#each $dataStore.headers as header}
+      <option value={header}>{header}</option>
+    {/each}
+  </select>
   
-  <!-- SVG element for the bar chart -->
-  <svg id="bar-chart"></svg>
-  
-  <style>
-    #bar-chart {
-      display: block;
-      margin: auto;
-    }
-  </style>
+  <div id="bar-chart-svg"></div>
   
